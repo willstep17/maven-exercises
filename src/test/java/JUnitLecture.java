6af29c55347e526bd0e3e0f5a335d6bb84e4e22f
William Stephens <william.a.stephens17@gmail.com>
2020-02-14 09:36:35 -0600

    JUnit lecture

--- src/test/java/JUnitLecture.java
+++ src/test/java/JUnitLecture.java
@@ -14,5 +14,23 @@ public class JUnitLecture {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
 
+        //Delta required when working with floating points. Specifies positive margin of error of decimals.
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+    }
+
+    @Test
+    public void testIfObjectsAreDifferent() {
+        Object dog = new Object();
+        Object sheep = new Object();
+        Object clonedSheep = sheep;
+        assertNotSame(sheep, dog);
+        assertSame(sheep, clonedSheep);
+    }
 }
